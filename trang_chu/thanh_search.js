4cd54ffacb44a0a9919a1d1d500853532568e1f8
thuong2211 <thuongphuongthang@gmail.com>
2023-10-26 00:43:03 +0700

    thêm lần hai

--- trang_chu/thanh_search.js
+++ trang_chu/thanh_search.js
@@ -2,17 +2,41 @@ function search(){
     var k = document.getElementById('kw');
     if( k != null ){
         k = k.value;
-        if(k == "bút" || k == "Bút"){
-            window.location.href = "./but/but_main.html";
+        if(k == "bút" || k == "Bút" || k == "BÚT"){
+            window.location.href = "https://tlchiii.github.io/Btlon/but.html?fbclid=IwAR26JX5XfcxTG8O7rFzm4AqNLQQOKjQEw7HnUplkYrGVE9MDdaOXLia86dQ";
         }
-        else if(k == "Vở" || k == "vở"){
-            window.location.href = "../vo/vo_main.html";
+        else if(k == "Vở" || k == "vở" || k == "VỞ"){
+            window.location.href = "https://tlchiii.github.io/Btlon/vo.html?fbclid=IwAR3a4nf5u4yQwq9lJu5jbfmzvfwpF6hb9rIAFIUpGhL3lT1FNzR9Kce0Cmk";
         }
-        else if(k == "Tẩy" || k == "tẩy"){
-            window.location.href = "./tay/tay_main.html";
+        else if(k == "Tẩy" || k == "tẩy" || k == "TẨY"){
+            window.location.href = "https://tlchiii.github.io/Btlon/tay.html?fbclid=IwAR0W6TvL931RpzmSW3UHmK8rbkg_gRQf53i1JHKdAbuzXtXunSpoWNZQqEk";
         }
-        else if(k == "Thước" || k == "thước"){
-            window.location.href = "./thuoc/thuoc_main.html";
+        else if(k == "Thước" || k == "thước" || k == "THƯỚC"){
+            window.location.href = "https://tlchiii.github.io/Btlon/thuoc.html?fbclid=IwAR2tutt-FY_uwyMNXB1HDhEIS3roxyTUh-0Gs-gZcDamaQ9b3B42dJLD0qE";
+        }
+        else if(k == "Bút Bi Thiên Long" || k == "bút bi thiên long"){
+            window.location.href = "https://peoday123.github.io/BAITAPLON/btl/butbi.html?fbclid=IwAR224XCz408nU2ctaaYLU_rifkkZs1r6TpsbjE9pvJxyOo9MtDp5jjTcx2M";
+        }
+        else if(k == "Bút chì Thiên Long" || k == "bút chì thiên long"){
+            window.location.href = "https://peoday123.github.io/BAITAPLON/btl/butchi.html?fbclid=IwAR3jZf2HtcATl38mlcPnGp4Qt1c5I5n8I7gOHUSQOhx3MZwdqXT1Z27r7m0";
+        } 
+        else if(k == "Gôm tẩy xóa chì" || k == "gôm tẩy bút chì"){
+            window.location.href = "https://peoday123.github.io/BAITAPLON/btl/tay.html?fbclid=IwAR0rkMaBdOZolQ1cm9wIj0S1cR2Zsqw4CXPe1tS1L3ccSvp3g92xkfXmGno";
+        }
+        else if(k == "Gôm tẩy hình gấu" || k == "gôm tẩy hình gấu"){
+            window.location.href = "https://peoday123.github.io/BAITAPLON/btl/taymau.html?fbclid=IwAR1Okr_B3Xj3qIDkePFpiTpoSvCPHnP-NUiCKlt4iob8dEcs8VbtkI-udlg";
+        }
+        else if(k == "Thước dẻo PP" || k == "thước dẻo PP"){
+            window.location.href = "https://peoday123.github.io/BAITAPLON/btl/thuocdeo.html?fbclid=IwAR2F_ifd6gfM6IdIztKu56HEs90qsBw-HiLoYLprRLOhYeSpEQXUhNEYdLI";
+        }
+        else if(k == "Thước thẳng" || k == "thước thẳng"){
+            window.location.href = "https://peoday123.github.io/BAITAPLON/btl/thuocthang.html?fbclid=IwAR0CLRTL8fAS_i9aVj5urbxJFQ6G6o7BLX6bt_Q8_VEUZr6s3E9KyMdzK8Y";
+        }
+        else if(k == "Vở ô li" || k == "vở ô li"){
+            window.location.href = "https://peoday123.github.io/BAITAPLON/btl/vooly.html?fbclid=IwAR1uKacEOoik-lzcqy7F_cmyh-KQSAaNb0Zva0X25a3mKxJtyUwa5ir_Fec";
+        }
+        else if(k == "Vở viết IVORY" || k == "vở viết IVORY"){
+            window.location.href = "https://peoday123.github.io/BAITAPLON/btl/vongang.html?fbclid=IwAR0pmyny0j3VvmwKJItEb4FdEJzQGNNV67k8SpWtptPz8xuMFx0aezD76Hs";
         }
         else{
             alert("Không có sản phẩm mà bạn muốn tìm");
